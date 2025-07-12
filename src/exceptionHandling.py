b0dc4053630eb9fd12c1f4d5dfa49c9a6cb11138
xavierdsml <guptaatusharr@gmail.com>
2025-07-12 10:36:00 +0530

    update

--- src/exceptionHandling.py
+++ src/exceptionHandling.py
@@ -14,7 +14,7 @@ def error_message_detail(error, error_detail:sys):
 
 
 
-class customException(Exception):
+class CustomException(Exception):
   def __init__(self, error_message, error_detail:sys):
     super().__init__(error_message)
 
