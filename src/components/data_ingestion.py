830c42ddbe60e889ff268e819d867c01557c4026
xavierdsml <guptaatusharr@gmail.com>
2025-07-12 10:49:10 +0530

    data_ingestion.udpdation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -46,6 +46,6 @@ class DataIngestion:
       raise CustomException(e, sys)
     
 
-if __name__=="__main__":
-  obj = DataIngestion()
-  obj.initiate_data_ingestion
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
